6b9b708a4f962c94a81effd4288ecc50b68af49f
Abdo Yasser <abdoyasser2018a@gmail.com>
2024-04-22 19:30:07 +0200

    completed todos

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+returns information about employee's TO DO list.
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    employee_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com"
+
+    user = requests.get(f"{url}/users/{employee_id}").json()
+    todos = requests.get(f"{url}/todos", params={'userId': employee_id}).json()
+
+    completed_tasks = []
+    for task in todos:
+        if task.get("completed"):
+            completed_tasks.append(task)
+
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed_tasks), len(todos)))
+    for todo in completed_tasks:
+        print("\t" + todo.get("title"))
