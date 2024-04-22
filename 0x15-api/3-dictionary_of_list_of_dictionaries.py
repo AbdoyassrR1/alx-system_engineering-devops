34fc3495bae19a9ae669a7cfdccf7b77c05635cf
Abdo Yasser <abdoyasser2018a@gmail.com>
2024-04-23 01:45:36 +0200

    all data

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+export data in the JSON format.
+"""
+import json
+import requests
+
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com"
+
+    users = requests.get(f"{url}/users").json()
+    with open("todo_all_employees.json", "w") as file:
+        json.dump(
+            {
+                user.get("id"): [
+                    {
+                        "task": todo.get("title"),
+                        "completed": todo.get("completed"),
+                        "username": user.get("username")
+                    }
+                    for todo in requests.get(f"{url}/todos",
+                                            params={"userId": user.get("id")})
+                    .json()
+                ]
+                for user in users
+            },
+            file
+        )
