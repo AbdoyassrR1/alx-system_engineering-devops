73d76bb731ca573d5a29345f001702488395b3f2
Abdo Yasser <abdoyasser2018a@gmail.com>
2024-05-08 02:32:04 +0300

    update

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Module for task 3"""
+import requests
 
 
 def count_words(subreddit, word_list, word_count={}, after=None):
@@ -9,20 +10,20 @@ def count_words(subreddit, word_list, word_count={}, after=None):
     given keywords (case-insensitive, delimited by spaces. Javascript
     should count as javascript, but java should not).
     """
-    import requests
 
-    sub_info = requests.get("https://www.reddit.com/r/{}/hot.json"
+    subscribers = requests.get(
+                            "https://www.reddit.com/r/{}/hot.json"
                             .format(subreddit),
                             params={"after": after},
                             headers={"User-Agent": "My-User-Agent"},
                             allow_redirects=False)
-    if sub_info.status_code != 200:
+    if subscribers.status_code != 200:
         return None
 
-    info = sub_info.json()
+    data = subscribers.json()
 
     hot_l = [child.get("data").get("title")
-             for child in info
+             for child in data
              .get("data")
              .get("children")]
     if not hot_l:
@@ -40,11 +41,11 @@ def count_words(subreddit, word_list, word_count={}, after=None):
                 if s_word.lower() == word.lower():
                     word_count[word] += 1
 
-    if not info.get("data").get("after"):
+    if not data.get("data").get("after"):
         sorted_counts = sorted(word_count.items(), key=lambda kv: kv[0])
         sorted_counts = sorted(word_count.items(),
                                key=lambda kv: kv[1], reverse=True)
         [print('{}: {}'.format(k, v)) for k, v in sorted_counts if v != 0]
     else:
         return count_words(subreddit, word_list, word_count,
-                           info.get("data").get("after"))
+                           data.get("data").get("after"))
